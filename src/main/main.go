cc3ab8860fe568cdc208ac1ddd6bc73d2364da1c
tengrommel <wwwsousoucom@126.com>
2017-12-16 05:05:49 +0800

    second

--- src/main/main.go
+++ src/main/main.go
@@ -10,9 +10,32 @@ func yallo(context echo.Context) error {
 	return context.String(http.StatusOK, "yallo from the web side!")
 }
 
+func getCats(context echo.Context) error {
+	catName := context.QueryParam("name")
+	catType := context.QueryParam("type")
+
+	dataType := context.Param("data")
+
+	if dataType == "string"{
+		return context.String(http.StatusOK, fmt.Sprintf("your cat name is: %s\nand his type is: %s\n", catName, catType))
+	}
+	if dataType == "json"{
+		return context.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+	return context.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you need to lets us know if you want json or string data",
+	})
+}
+
 func main()  {
 	fmt.Println("Welcome to the server")
 	e := echo.New()
+
 	e.GET("/", yallo)
+	e.GET("/cats/:data", getCats)
+
 	e.Start(":8000")
 }
