2cd0a17ab1ea5b0fce9059984d0391597b576dec
tengrommel <wwwsousoucom@126.com>
2017-12-16 10:19:02 +0800

    jwt

--- src/main/main.go
+++ src/main/main.go
@@ -11,6 +11,7 @@ import(
 	"github.com/labstack/echo/middleware"
 	"time"
 	"strings"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 type Cat struct {
@@ -26,6 +27,11 @@ type Dog struct {
 type Hamster struct {
 	Name        string    `json:"name"`
 	Type        string    `json:"type"`
+}
+
+type JwtClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
 }
 
 func yallo(c echo.Context) error {
@@ -121,9 +127,35 @@ func login(c echo.Context) error {
 		cookie.Expires = time.Now().Add(48 * time.Hour)
 
 		c.SetCookie(cookie)
-		return c.String(http.StatusOK, "You were logged in!")
+		
+		token, err := createJwtToken()
+		if err != nil{
+			log.Println(err)
+			return c.String(http.StatusInternalServerError, "something went wrong")
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "You were logged in!",
+			"token": token,
+		})
 	}
 	return c.String(http.StatusUnauthorized, "Your username or password were wrong")
+}
+
+func createJwtToken() (string, error) {
+	claims := JwtClaims{
+		"jack",
+		jwt.StandardClaims{
+			Id: "main_user_id",
+			ExpiresAt: time.Now().Add(24*time.Hour).Unix(),
+		},
+	}
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	token, err := rawToken.SignedString([]byte("mySecret"))
+	if err != nil{
+		return "", err
+	}
+	return token, nil
 }
 
 // middlewares//
